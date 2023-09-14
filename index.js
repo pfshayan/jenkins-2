5da9bd853185063889ea83520b4f4e51a9c02604
Muhammad Shahyan <muhammadshahyan@HammadHassan-2.local>
2023-09-14 17:32:41 +0500

    jenkins-2

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, CI/CD World!\n');
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
